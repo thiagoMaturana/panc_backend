2f8c614ae3cb34ce72e32e208a808d091d199b54
Thiago Maturana <thiagomaturanarib@gmail.com>
2021-01-28 19:26:59 -0400

    cadastro de plantas

--- resources/views/publico/layout.blade.php
+++ resources/views/publico/layout.blade.php
@@ -92,7 +92,7 @@
             @else
             <a href="{{ route('logout') }}" onclick="event.preventDefault(); 
                         document.getElementById('logout-form').submit();"><i class='bx bx-log-out'></i>
-              {{ _('Logout') }}
+                Logout
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
